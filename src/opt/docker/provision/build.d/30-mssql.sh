29e4f761fe1b24ac137da3d8dff26e02c200c395
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2019-04-02 23:43:26 +0200

    add php-extensions sqlsrv, pdo_sqlsrv

---
+++ src/opt/docker/provision/build.d/30-mssql.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/debian/9/prod.list > /etc/apt/sources.list.d/mssql-release.list
+
+ACCEPT_EULA=Y apt-install msodbcsql17 unixodbc-dev mssql-tools
+pecl install sqlsrv pdo_sqlsrv
+docker-php-ext-enable sqlsrv pdo_sqlsrv
