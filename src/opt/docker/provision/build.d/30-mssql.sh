0a4026388600508b11221b66dc0237ea0b2b928a
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2019-04-12 19:03:14 +0200

    fix mssql installation on app:7.0

--- src/opt/docker/provision/build.d/30-mssql.sh
+++ src/opt/docker/provision/build.d/30-mssql.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-[[ $(docker-image-info dist-codename) = "jessie" ]] || {
+function main() {
     curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
     curl https://packages.microsoft.com/config/debian/9/prod.list > /etc/apt/sources.list.d/mssql-release.list
 
     ACCEPT_EULA=Y apt-install msodbcsql17 unixodbc-dev mssql-tools
 
-    pecl install sqlsrv pdo_sqlsrv
+    local version=""
+    [[ $(php -v | head -n 1 | cut -d " " -f 2 | cut -f1-2 -d".") = "7.0" ]] && version="-5.3.0"
+    pecl install sqlsrv${version} pdo_sqlsrv${version}
     docker-php-ext-enable sqlsrv pdo_sqlsrv
 }
+
+[[ $(docker-image-info dist-codename) = "jessie" ]] || main
