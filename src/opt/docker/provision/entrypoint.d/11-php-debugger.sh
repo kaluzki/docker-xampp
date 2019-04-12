db9bc9b4010ff7390a22f7f0db79e3e55161042d
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2019-04-12 12:42:03 +0200

    debugger extensions are removed only in prod environment

--- src/opt/docker/provision/entrypoint.d/11-php-debugger.sh
+++ src/opt/docker/provision/entrypoint.d/11-php-debugger.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-for CONF_PATH in ${PHP_CONF_PATHS}; do
-    rm -f "${CONF_PATH}"/xdebug.ini
-    rm -f "${CONF_PATH}"/blackfire.ini
-done
+if [[ ${DOCKER_XAMPP_BIN_ENV} == "prod" ]]; then
+    for CONF_PATH in ${PHP_CONF_PATHS}; do
+        rm -f "${CONF_PATH}"/xdebug.ini
+        rm -f "${CONF_PATH}"/blackfire.ini
+    done
+fi
