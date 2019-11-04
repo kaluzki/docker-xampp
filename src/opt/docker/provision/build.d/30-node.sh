0f4ed26668c90a110832ace73d2fbcd295c26697
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2019-11-04 08:43:52 +0100

    upgrade app containers to node 12.x

--- src/opt/docker/provision/build.d/30-node.sh
+++ src/opt/docker/provision/build.d/30-node.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-curl -sL https://deb.nodesource.com/setup_11.x | bash -
+curl -sL https://deb.nodesource.com/setup_12.x | bash -
 apt-install autoconf libtool libpng-dev nasm nodejs
 npm install --global npm @quasar/cli @vue/cli
