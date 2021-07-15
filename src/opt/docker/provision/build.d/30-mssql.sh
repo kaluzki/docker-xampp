42f615532eb477925d2dcd06f854b989e6fc2289
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2021-07-15 23:05:55 +0200

    fix mssql extensions for app:7.4 (buster) images

--- src/opt/docker/provision/build.d/30-mssql.sh
+++ src/opt/docker/provision/build.d/30-mssql.sh
@@ -2,7 +2,7 @@
 
 function main() {
     curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-    curl https://packages.microsoft.com/config/debian/9/prod.list > /etc/apt/sources.list.d/mssql-release.list
+    curl https://packages.microsoft.com/config/debian/10/prod.list > /etc/apt/sources.list.d/mssql-release.list
 
     ACCEPT_EULA=Y apt-install msodbcsql17 unixodbc-dev mssql-tools
 
@@ -10,9 +10,10 @@ function main() {
     [[ $(php -v | head -n 1 | cut -d " " -f 2 | cut -f1-2 -d".") = "7.0" ]] && version="-5.3.0"
     pecl install sqlsrv${version} pdo_sqlsrv${version}
     docker-php-ext-enable sqlsrv pdo_sqlsrv
+
+    go-replace --mode=lineinfile --regex -s 'DEFAULT@SECLEVEL=2' -r 'DEFAULT@SECLEVEL=1' /etc/ssl/openssl.cnf
 }
 
-# todo 7.4 (buster)
-if [[ $(docker-image-info dist-codename) = "stretch" ]]; then
+if [[ $(docker-image-info dist-codename) = "buster" ]]; then
     main
 fi
