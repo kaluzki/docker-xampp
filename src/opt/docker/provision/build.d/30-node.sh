4e9faee8ee5417ff2793ee11d0b7adac6874398a
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2021-10-15 13:21:22 +0200

    upgrade node version from 14 to 16

--- src/opt/docker/provision/build.d/30-node.sh
+++ src/opt/docker/provision/build.d/30-node.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-curl -sL https://deb.nodesource.com/setup_14.x | bash -
+curl -sL https://deb.nodesource.com/setup_16.x | bash -
 apt-install autoconf libtool libpng-dev nasm nodejs
 npm install --global npm
