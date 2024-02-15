32f6ae05b223350123b66c10a2a14d2327344177
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2024-02-15 22:20:22 +0100

    Fix use of `DOCKER_XAMPP_BIN_ENV`
    This commit corrects the syntax in conditional statement of PHP's debugger shell script. Specifically, the previous syntax used for the default value substitution in bash script was not correct. This is now resolved by using colon "-", hence following proper bash syntax rules for default parameters.

--- src/app/opt/docker/provision/entrypoint.d/11-php-debugger.sh
+++ src/app/opt/docker/provision/entrypoint.d/11-php-debugger.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ ${DOCKER_XAMPP_BIN_ENV-} == "prod" ]]; then
+if [[ ${DOCKER_XAMPP_BIN_ENV:-} == "prod" ]]; then
     for CONF_PATH in ${PHP_CONF_PATHS}; do
         rm -f "${CONF_PATH}"/*xdebug.ini
         rm -f "${CONF_PATH}"/*blackfire.ini
