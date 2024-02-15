4e6e7ce30691a615845733ed76c8b32750969221
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2024-02-15 22:23:14 +0100

    Update MSSQL provision script for "buster"
    This commit updates the MSSQL provision script for Docker 'buster' image. The changes support php 7.4 and fixed the version specific pecl installation for sqlsrv and pdo_sqlsrv. The function main has also been eliminated for a sequential scripting style.

--- src/app/opt/docker/provision/build.d/30-mssql.sh
+++ src/app/opt/docker/provision/build.d/30-mssql.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-function main() {
+set -e
+
+if [[ $(docker-image-info dist-codename) = "buster" ]]; then
+
     curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
     curl https://packages.microsoft.com/config/debian/10/prod.list > /etc/apt/sources.list.d/mssql-release.list
 
     ACCEPT_EULA=Y apt-install msodbcsql17 unixodbc-dev mssql-tools
 
     local version=""
-    [[ $(php -v | head -n 1 | cut -d " " -f 2 | cut -f1-2 -d".") = "7.0" ]] && version="-5.3.0"
+    [[ $(php -v | head -n 1 | cut -d " " -f 2 | cut -f1-2 -d".") = "7.4" ]] && version="-5.10.1"
+
     pecl install sqlsrv${version} pdo_sqlsrv${version}
     docker-php-ext-enable sqlsrv pdo_sqlsrv
 
     go-replace --mode=lineinfile --regex -s 'DEFAULT@SECLEVEL=2' -r 'DEFAULT@SECLEVEL=1' /etc/ssl/openssl.cnf
-}
-
-if [[ $(docker-image-info dist-codename) = "buster" ]]; then
-    main
 fi
